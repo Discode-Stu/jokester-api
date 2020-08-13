e88a435b2c0611e0330ca9164f638ac87574ba35
Discode Stu <dstuart85@yahoo.com>
2020-08-13 15:46:28 -0400

    added database uri

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
-import os 
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,8 +10,7 @@ CORS(app)
 # def hello():
 #     return "Hey Flask"
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
