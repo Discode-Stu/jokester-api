8e1a6da8f028f1697eeedf72c6b9e00d3bb0c091
Discode Stu <dstuart85@yahoo.com>
2020-08-25 15:20:13 -0400

    added app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ CORS(app)
 #     return "Hey Flask"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
