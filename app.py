d33c3eebc852a86f030c0f914b1f5e95c476a8b0
Discode Stu <dstuart85@yahoo.com>
2020-09-12 10:42:20 -0400

    added user database

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import os
+import bcrypt
 
 app = Flask(__name__) 
 CORS(app)
@@ -17,6 +18,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+# SQLAlchemy.create_all() 
 
 class Joke(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -91,6 +94,85 @@ def joke_delete(id):
 
     # return joke_schema.jsonify(joke)
     return "Joke was successfully deleted"
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(100), unique=False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id','name', 'email', 'password')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+# Endpoint to create a new user
+@app.route('/user', methods=["POST"])
+def add_user():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+
+    new_user = User(name, email, password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    user = User.query.get(new_user.id)
+
+    return user_schema.jsonify(user)
+
+
+# Endpoint to query all users
+@app.route("/users", methods=["GET"])
+def get_users():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result)
+
+
+# Endpoint for querying a single user
+@app.route("/user/<id>", methods=["GET"])
+def get_user(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
+
+
+# Endpoint for updating a user
+@app.route("/user/<id>", methods=["PUT"])
+def user_update(id):
+    user = User.query.get(id)
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+
+    user.name = name
+    user.email = email
+    user.password = password
+
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+
+# Endpoint for deleting a record
+@app.route("/user/<id>", methods=["DELETE"])
+def user_delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    # return user_schema.jsonify(user)
+    return "User was successfully deleted"
 
 
 
